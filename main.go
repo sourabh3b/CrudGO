c01c6e4be84fbc6daa1dc8973fcc49059a715b6d
sourabh <sourabhb@buffalo.edu>
2018-03-26 21:05:39 -0400

    added API for delete user from database, handled case where username already exist in the database

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"net/http"
-	"fmt"
+	"github.com/unrolled/render"
+	"github.com/CrudGO/user"
+	"github.com/gorilla/mux"
+	"encoding/json"
 )
-
 
 //TestRoute - test route
 func TestRoute(w http.ResponseWriter, r *http.Request) {
@@ -15,10 +18,124 @@ func TestRoute(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
+/*
+GetAllUsers - §  Return a list of all users as a JSON string
+e.g.:
+{ "users":
+    [{ "username": "jsmith", "displayName": "John Smith", "department": "Sales" },
+    { "username": "jdoe", "displayName": "John Doe", "department": "Development" }]
+}
+*/
 
-func main()  {
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	render := render.New()
+	users,err := user.GetAllUsers()
 
-	log.Info("Started CRUD....")
+	//check for error, if there is error then return 500 status code with empty user, else return status 200 with all users
+	if(err != nil){
+		render.JSON(w,http.StatusInternalServerError, users)
+	}else{
+		render.JSON(w,http.StatusOK, users)
+	}
+
+}
+
+/*
+GetUserByName - Return the data of a particular user as a JSON string or status code 404 if not found
+e.g.:
+ { "displayName": "John Smith", "department": "Sales" }
+*/
+func GetUserByName(w http.ResponseWriter, r *http.Request) {
+	render := render.New()
+
+	//getting username from the API url
+	vars := mux.Vars(r)
+	username := vars["username"]
+
+	fmt.Fprintf(w, "You've requested the username: %s\n", username)
+
+
+	log.Info("username >>>>>.",username)
+	user,err := user.GetUserByName(username);
+
+	//check for error, if there is error then return 500 status code with empty user, else return status 200 with all users
+	if(err != nil){
+		render.JSON(w,http.StatusForbidden, user)
+	}else{
+		render.JSON(w,http.StatusOK, user)
+	}
+
+
+	return
+}
+
+//InsertUser - test route
+func InsertUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	render := render.New()
+
+	inputUser := user.User{}
+
+	insertAPIResponse := user.Response{}
+
+
+	//decoding the request into team, so that it can be used to save the team details
+	err := json.NewDecoder(r.Body).Decode(&inputUser)
+	if err != nil {
+		insertAPIResponse.Message = "Invalid input : Unable to parse input body"
+		insertAPIResponse.Status = http.StatusBadRequest
+		render.JSON(w,http.StatusBadRequest, insertAPIResponse)
+		return
+	}
+
+
+	err, exist  := user.InsertNewUser(inputUser)
+
+	//check if user already exist in the database
+	if exist {
+		insertAPIResponse.Message = "User already found in the database"
+		insertAPIResponse.Status = http.StatusConflict
+		render.JSON(w,http.StatusConflict, insertAPIResponse)
+		return
+	}
+
+	//check for error returned from InsertNewUser function
+	if(err != nil){
+		insertAPIResponse.Message = "Unable to insert user into database"
+		insertAPIResponse.Status = http.StatusForbidden
+		render.JSON(w,http.StatusForbidden, insertAPIResponse)
+	}else{
+		insertAPIResponse.Message = "Inserted user into database"
+		insertAPIResponse.Status = http.StatusOK
+		render.JSON(w,http.StatusOK, insertAPIResponse)
+	}
+
+
+	return
+}
+
+//DeleteUser - test route
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	//render := render.New()
+	fmt.Fprint(w, "Hello World !")
+	//render.JSON(w, http.StatusOK, nil)
+	return
+}
+
+
+func main() {
+
+	log.Info("Started CRUD following are the APIs available....")
+
+	//router for all APIs
 	http.HandleFunc("/test", TestRoute)
+	http.HandleFunc("/users", GetAllUsers) //Get all users present in the database
+	http.HandleFunc("/insert", InsertUser) //inserts a user into database
+	http.HandleFunc("/users/{username}", GetUserByName) //get user by user name
+	//http.HandleFunc("/users/{username}", DeleteUser) //Deletes user from database
+
+	//listening on 8889 port number
 	http.ListenAndServe(":8889", nil)
 }
---
+++ user/user.go
@@ -0,0 +1,105 @@
+package user
+
+import (
+	"errors"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+//User - data model for a user
+type User struct {
+	UserName    string `bson:"username" json:"username"`       // username of user example :jsmith
+	DisplayName string `bson:"displayName" json:"displayName"` // displayName of user example :John Smith
+	Department  string `bson:"department" json:"department"`   // department for the user example :Sales
+}
+
+//Response - data model for API response
+type Response struct {
+	Status  int     `bson:"status" json:"status"`
+	Message string  `bson:"message" json:"message"`
+}
+
+
+
+//GetAllUsers -  return all users present in the database
+func GetAllUsers() ([]User, error) {
+
+	//initialising user array object to be returned when calling this function
+	users := []User{}
+
+	session, err := mgo.Dial("127.0.0.1")
+	if err != nil {
+		log.Println("Mongo error", err.Error())
+		return users, errors.New("Mongo connection Error " + err.Error())
+	}
+	defer session.Close()
+
+	//mongo query to get all users in myDB database and User table
+	err = session.DB("myDB").C("User").Find(nil).All(&users) //todo: move constant string to a constant file //todo: rename db name
+
+	return users, err
+}
+
+
+//GetUserByName -  return all users present in the database
+func GetUserByName(username string) (User, error) {
+
+	//initialising user array object to be returned when calling this function
+	user := User{}
+
+	session, err := mgo.Dial("127.0.0.1")
+	if err != nil {
+		log.Println("Mongo error", err.Error())
+		return user, errors.New("Mongo connection Error " + err.Error())
+	}
+	defer session.Close()
+
+	//mongo query to get all users in myDB database and User table
+	err = session.DB("myDB").C("User").Find(bson.M{"username": username}).One(&user)
+
+	return user, err
+}
+
+
+//InsertNewUser -  return all users present in the database
+func InsertNewUser(user User) (error, bool) {
+
+	session, err := mgo.Dial("127.0.0.1")
+	if err != nil {
+		log.Println("Mongo error", err.Error())
+		return errors.New("Mongo connection Error " + err.Error()), false
+	}
+	defer session.Close()
+
+	//check if user already exist
+	err = session.DB("myDB").C("User").Find(bson.M{"username": user.UserName}).One(&user)
+	if err == nil {
+		return nil, true
+	}
+
+	//mongo query to insert new user
+	err = session.DB("myDB").C("User").Insert(user)
+
+	return  err, false
+}
+
+
+//DeleteUser -  return all users present in the database
+func DeleteUser(username string) (error) {
+
+	//initialising user array object to be returned when calling this function
+	session, err := mgo.Dial("127.0.0.1")
+	if err != nil {
+		log.Println("Mongo error", err.Error())
+		return errors.New("Mongo connection Error " + err.Error())
+	}
+	defer session.Close()
+
+	//mongo query to get all users in myDB database and User table
+	err = session.DB("myDB").C("User").Remove(bson.M{"username": username})
+
+	return err
+}
+
+
